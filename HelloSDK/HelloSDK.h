a77696302d9152c93843c9ecdfc4e9104dcf9161
Oleg Naumenko <olegnowmenco@gmail.com>
2021-06-10 19:42:47 +0300

    Initial Commit

---
+++ HelloSDK/HelloSDK.h
@@ -0,0 +1,18 @@
+//
+//  HelloSDK.h
+//  HelloSDK
+//
+//  Created by oleg.naumenko on 10.06.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HelloSDK.
+FOUNDATION_EXPORT double HelloSDKVersionNumber;
+
+//! Project version string for HelloSDK.
+FOUNDATION_EXPORT const unsigned char HelloSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloSDK/PublicHeader.h>
+
+
